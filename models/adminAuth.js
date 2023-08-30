222a47ff634c76af9e129e6cb7fe4161e6e8202c
Saimall <rameshwaramsaimallik@gmail.com>
2023-08-30 23:07:06 +0530

    Updated model changes and routes

--- models/adminAuth.js
+++ models/adminAuth.js
@@ -2,10 +2,24 @@
 const mongoose = require('mongoose');
 
 const adminSchema = new mongoose.Schema({
-  adminId: { type: String, required: true, unique: true },
+
+  name: { type: String, required: true, unique: true },
+  phonenumber:{type:Number,required:true,unique:true},
+  email :{ type: String, required: true, unique: true },
   password: { type: String, required: true },
+  role :{type:String ,required:true, enum:['superadmin','supportteam','dutymanager','approvalteam']}
+
 });
 
 const Admin = mongoose.model('Admin', adminSchema);
 
 module.exports = Admin;
+
+// name,phonenumber,email,password,role {
+      //superadmin
+      //supportteam - service histroy
+      ////dutymanager
+       //approval team - pending request, partnerassigned
+     
+//}
+
